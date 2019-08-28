9254ffe5536f4f4d2fc25cdc94ffec25ad820da8
Mateusz Odroniec <mateusz.odr@gmail.com>
2019-08-29 00:36:03 +0200

    Refactor & Adding comments

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
 const fibonacciCounter = function fibonacci(result, length) {
 
-    if (result.length >= length) {
+    if (result.length >= length) { // sprawdzam warunek, jeżeli ilość elementów w tablicy jest >= przypisanej wartości length to zwróc tablicę, jeżeli nie przejdz dalej
         return result;
     }
-    result.push(result[result.length - 2] + result[result.length - 1]);
-    return fibonacci(result, length);
-
+    result.push(result[result.length - 2] + result[result.length - 1]); // dodaj do tablicy sumę ostatniego i przedostatniego elementu
+    return fibonacci(result, length); // wywołujemy funkcję rekurencyjną do momentu aż spełnimy warunek długość tablicy >= length
 };
 
-const counter = function counter(length) {
-    console.time('Calling Time');
-    console.timeEnd('Calling Time');
-    return fibonacciCounter([0, 1], length);
-}
+const counter = (length) => {
+    return fibonacciCounter([0, 1], length); // pierwszy parametr to tablica startowa z elementem 0 o wartości 0 oraz elementem 1 o wartości 1, drugi parametr to ilość elementów jakie chcemy zwrócić
+};
 
+console.time('Calling Time');
 console.log(counter(15));
+console.timeEnd('Calling Time'); // wywołanie czasu potrzebnego do wykonania funkcji
